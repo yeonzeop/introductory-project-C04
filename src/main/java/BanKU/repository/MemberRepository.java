16ee534e7f7d14d20aaefbc31c20c906ef8aa674
yeonzeop <ksp2617@naver.com>
2025-06-11 11:07:11 +0900

    fix: 적금 계좌 제외 조건문 추가

--- src/main/java/BanKU/repository/MemberRepository.java
+++ src/main/java/BanKU/repository/MemberRepository.java
@@ -219,7 +219,7 @@ public class MemberRepository {
     public void freeAccountInterest(long diffMonths) {
         double interestRate = 0.1;
         for (Account account : accounts.values()) {
-            if (account.isActive()) {
+            if (account.isActive() && !(account instanceof SavingAccount)) {
                 long interest = (long) Math.ceil(account.getBalance() * (diffMonths * ((interestRate / 100) / 12))); // 소수 첫째자리에서 올림
                 // 확인용 출력
 //                System.out.println("이자: " + interest);
